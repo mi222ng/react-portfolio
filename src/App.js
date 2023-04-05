174b9d4f0327196bd7213fae4852979f9bd33c7a
mi222ng <mi222ng@naver.com>
2023-04-05 11:03:46 +0900

    생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { Reset } from 'styled-reset';
 import './App.css';
+import React, { Component } from 'react'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <Reset />
+      <header className="main_contents">
+        <div className="container">
+          <Nav></Nav>
+          <About></About>
+          <Skills></Skills>
+          <Works></Works>
+          <Contact></Contact>
+        </div>
       </header>
     </div>
   );
 }
+
+// 메뉴
+class Nav extends Component {
+  render() {
+    return (
+      <div className='menu'>
+        <nav>
+          <ul>
+              <li><a href="">About</a></li>
+              <li><a href="">Skills</a></li>
+              <li><a href="">Works</a></li>
+              <li><a href="">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+}
+class About extends Component {
+  render() {
+    return (
+      <section>
+        <h2 className='title'>About me<span role="img" aria-label="Smiling Face">😊</span></h2>
+      </section>
+    )
+  }
+}
+class Skills extends Component {
+  render() {
+    return (
+      <section>
+        <h2 className='title'>Improving skills<span role="img" aria-label="Pencil">✏️</span></h2>
+      </section>
+    )
+  }
+}
+class Works extends Component {
+  render() {
+    return (
+      <section>
+        <h2 className='title'>It’s my works<span role="img" aria-label="Flexed Biceps">💪</span></h2>
+      </section>
+    )
+  }
+}
+class Contact extends Component {
+  render() {
+    return (
+      <section>
+        <h2 className='title'>Contact me here!<span role="img" aria-label="Backhand Index Pointing Down">👇</span></h2>
+      </section>
+    )
+  }
+}
+
+
 
 export default App;
