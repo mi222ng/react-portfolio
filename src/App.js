262c6c2af526414e588d84249017aef1110207a6
mi222ng <mi222ng@naver.com>
2023-04-14 13:00:11 +0900

    파비콘 수정 및 이미지 분류

--- src/App.js
+++ src/App.js
@@ -64,12 +64,12 @@ class Skills extends Component {
       <section className='section' id='skills'>
         <h2 className='title'>Improving skills<span role="img" aria-label="Pencil">✏️</span></h2>
         <ul className='d-flex justify-content-center flex-wrap'>
-          <li><img src='/images/sk-html.png' alt='html'/></li>
-          <li><img src='/images/sk-css.png' alt='css'/></li>
-          <li><img src='/images/sk-javascript.png' alt='javascript'/></li>
-          <li><img src='/images/sk-bootstrap.png' alt='bootstrap'/></li>
-          <li><img src='/images/sk-jquery.png' alt='jquery'/></li>
-          <li><img src='/images/sk-react.png' alt='react'/></li>
+          <li><img src='/images/skills/sk-html.png' alt='html'/></li>
+          <li><img src='/images/skills/sk-css.png' alt='css'/></li>
+          <li><img src='/images/skills/sk-javascript.png' alt='javascript'/></li>
+          <li><img src='/images/skills/sk-bootstrap.png' alt='bootstrap'/></li>
+          <li><img src='/images/skills/sk-jquery.png' alt='jquery'/></li>
+          <li><img src='/images/skills/sk-react.png' alt='react'/></li>
         </ul>
       </section>
     )
