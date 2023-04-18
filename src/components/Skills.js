5469b933c613f6940685255a4703f3945c675f4a
mi222ng <mi222ng@naver.com>
2023-04-18 12:35:35 +0900

    절대경로 변경

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 
 class Skills extends Component {
   render() {
+    let publicURL = process.env.PUBLIC_URL;
     return (
       <section className='section' id='skills'>
         <h2 className='title'>Improving skills<span role="img" aria-label="Pencil">✏️</span></h2>
         <ul className='d-flex justify-content-center flex-wrap'>
-          <li><img src={process.env.PUBLIC_URL+'/images/skills/sk-html.png'} alt='html'/></li>
-          <li><img src={process.env.PUBLIC_URL+'/images/skills/sk-css.png'} alt='css'/></li>
-          <li><img src={process.env.PUBLIC_URL+'/images/skills/sk-javascript.png'} alt='javascript'/></li>
-          <li><img src={process.env.PUBLIC_URL+'/images/skills/sk-bootstrap.png'} alt='bootstrap'/></li>
-          <li><img src={process.env.PUBLIC_URL+'/images/skills/sk-jquery.png'} alt='jquery'/></li>
-          <li><img src={process.env.PUBLIC_URL+'/images/skills/sk-react.png'} alt='react'/></li>
+          <li><img src={publicURL+'/images/skills/sk-html.png'} alt='html'/></li>
+          <li><img src={publicURL+'/images/skills/sk-css.png'} alt='css'/></li>
+          <li><img src={publicURL+'/images/skills/sk-javascript.png'} alt='javascript'/></li>
+          <li><img src={publicURL+'/images/skills/sk-bootstrap.png'} alt='bootstrap'/></li>
+          <li><img src={publicURL+'/images/skills/sk-jquery.png'} alt='jquery'/></li>
+          <li><img src={publicURL+'/images/skills/sk-react.png'} alt='react'/></li>
         </ul>
       </section>
     )
