853f9df6126ded181e876c4b40c72bf12e5fd445
mi222ng <mi222ng@naver.com>
2023-04-18 12:18:23 +0900

    절대경로 변경

--- src/components/Works.js
+++ src/components/Works.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react'
 
 class Works extends Component {
   render() {
+    let publicURL = 'process.env.PUBLIC_URL';
     return (
       <section className='section' id='works'>
         <h2 className='title'>It’s my works<span role="img" aria-label="Flexed Biceps">💪</span></h2>
         <div className='d-flex flex-wrap'>
           <div>
-            <img src='/images/folder.svg' alt='circle' className='project1'/>
+            <img src={publicURL+'/images/folder.svg'} alt='circle' className='project1'/>
           </div>
           <div>
-            <img src='/images/folder.svg' alt='circle' className='project2'/>
+            <img src={publicURL+'/images/folder.svg'} alt='circle' className='project2'/>
           </div>
           <div>
-            <img src='/images/folder.svg' alt='circle' className='project3'/>
+            <img src={publicURL+'/images/folder.svg'} alt='circle' className='project3'/>
           </div>
           <div>
-            <img src='/images/folder.svg' alt='circle' className='project4'/>
+            <img src={publicURL+'/images/folder.svg'} alt='circle' className='project4'/>
           </div>
         </div>
       </section>
