7775b261bad0bd6dab3b6daf181626b42b69cc86
mi222ng <mi222ng@naver.com>
2023-04-18 12:08:20 +0900

    절대경로 수정

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class About extends Component {
   render() {
-    const profileUrl = "/images/profile.jpg";
+    const profileUrl = process.env.PUBLIC_URL+"/images/profile.jpg";
     return (
       <section className='section' id='about'>
         <h2 className='title'>About me<span role="img" aria-label="Smiling Face">😊</span></h2>
