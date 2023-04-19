8c8ed5506a8268c6a9be57086bbd7924751a49a3
mi222ng <mi222ng@naver.com>
2023-04-19 18:20:09 +0900

    data json 추가

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import WorksList from './WorksList';
 
 class Works extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      worksList : [] //초기 리스트는 비어있습니다.
+    };
+  }
+  componentDidMount(){
+    this._getList(); 
+  }
+  _getList(){
+    //worksList를 가지고 옵니다.
+  const apiUrl = 'data/worksdata.json';
+  axios.get(apiUrl)
+    .then(data => {
+      //가지고 온 리스트를 state에 저장합니다.
+      this.setState({
+        worksList : data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
   render() {
     let publicURL = process.env.PUBLIC_URL;
     return (
@@ -19,6 +45,7 @@ class Works extends Component {
           <div>
             <img src={publicURL+'/images/folder.svg'} alt='circle' className='project4'/>
           </div>
+          <WorksList data={this.worksList}/>
         </div>
       </section>
     )
---
+++ src/components/WorksList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const WorksList = (props)=>{
+  let data = props.data;
+    console.log(data);
+    // <ul>
+    //     { props.list.map((work, index) => (
+    //     <li key={index}>
+    //       <div>
+    //         <h4>{work.title}</h4>
+    //         <span>주제 : {work.theme}</span>
+    //         <span>구분 : {work.sort}</span>
+    //         <span>언어 : {work.lang}</span>
+    //         <a href={work.url}>바로가기</a>
+    //       </div>
+    //     </li>
+    //     )) }
+    // </ul>
+  //     return (
+  // )
+}
+
+
+export default WorksList;
