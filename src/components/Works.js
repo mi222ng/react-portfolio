8e1d4160ec2cfc45dedf6992574cd1ce0b7e5b15
mi222ng <mi222ng@naver.com>
2023-05-08 23:02:19 +0900

    list출력 업데이트중

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,54 +1,55 @@
-import React, { Component } from 'react'
-import axios from 'axios';
-import WorksList from './WorksList';
+import React, { Component } from "react";
+import axios from "axios";
+import WorksList from "./WorksList";
 
 class Works extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      worksList : [] //초기 리스트는 비어있습니다.
+      worksList: [], //초기 리스트는 비어있습니다.
+      index: "1",
     };
   }
-  componentDidMount(){
-    this._getList(); 
+  componentDidMount() {
+    this._getList();
   }
-  _getList(){
+  _getList() {
     //worksList를 가지고 옵니다.
-  const apiUrl = 'data/worksdata.json';
-  axios.get(apiUrl)
-    .then(data => {
-      //가지고 온 리스트를 state에 저장합니다.
-      this.setState({
-        worksList : data
+    const apiUrl = "data/worksdata.json";
+    axios
+      .get(apiUrl)
+      .then((data) => {
+        //가지고 온 리스트를 state에 저장합니다.
+        this.setState({
+          worksList: data.data.worksList,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
+  }
   render() {
-    let publicURL = process.env.PUBLIC_URL;
     return (
-      <section className='section' id='works'>
-        <h2 className='title'>It’s my works<span role="img" aria-label="Flexed Biceps">💪</span></h2>
-        <div className='d-flex flex-wrap'>
-          <div>
-            <img src={publicURL+'/images/folder.svg'} alt='circle' className='project1'/>
-          </div>
-          <div>
-            <img src={publicURL+'/images/folder.svg'} alt='circle' className='project2'/>
-          </div>
-          <div>
-            <img src={publicURL+'/images/folder.svg'} alt='circle' className='project3'/>
-          </div>
-          <div>
-            <img src={publicURL+'/images/folder.svg'} alt='circle' className='project4'/>
-          </div>
-          <WorksList data={this.worksList}/>
+      <section className="section" id="works">
+        <h2 className="title">
+          It’s my works
+          <span role="img" aria-label="Flexed Biceps">
+            💪
+          </span>
+        </h2>
+        <div className="d-flex flex-wrap">
+          {this.state.worksList.length > 0 ? (
+            <WorksList
+              list={this.state.worksList.filter(
+                (work) => work.index === this.state.id
+              )}
+            />
+          ) : (
+            <span>LOADING...</span>
+          )}
         </div>
       </section>
-    )
+    );
   }
 }
 
--- src/components/WorksList.js
+++ src/components/WorksList.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React from "react";
 
-const WorksList = (props)=>{
-  let data = props.data;
-    console.log(data);
-    // <ul>
-    //     { props.list.map((work, index) => (
-    //     <li key={index}>
-    //       <div>
-    //         <h4>{work.title}</h4>
-    //         <span>주제 : {work.theme}</span>
-    //         <span>구분 : {work.sort}</span>
-    //         <span>언어 : {work.lang}</span>
-    //         <a href={work.url}>바로가기</a>
-    //       </div>
-    //     </li>
-    //     )) }
-    // </ul>
-  //     return (
-  // )
-}
-
+const WorksList = (props) => {
+  let publicURL = process.env.PUBLIC_URL;
+  return (
+    <div>
+      <ul>
+        {props.list &&
+          props.list.map((work, index) => (
+            // <li
+            //   className="worksbg"
+            //   key={index}
+            //   style={{
+            //     backgroundImage: `url(${
+            //       process.env.PUBLIC_URL + "/images/folder.svg"
+            //     })`,
+            //   }}
+            // >
+            //   <div>
+            //     <h4>{work.title}</h4>
+            //     <p>주제 : {work.theme}</p>
+            //     <p>구분 : {work.sort}</p>
+            //     <p>언어 : {work.lang}</p>
+            //     <a href={work.url}>바로가기</a>
+            //   </div>
+            // </li>
+            <li className="workslist" key={index}>
+              <div className="worksbg">
+                <img
+                  src={publicURL + "/images/folder.svg"}
+                  alt="circle"
+                  className="project1"
+                />
+                <h4 className="workstt">{work.title}</h4>
+              </div>
+              <div>
+                <p>주제 : {work.theme}</p>
+                <p>구분 : {work.sort}</p>
+                <p>언어 : {work.lang}</p>
+                <a href={work.url}>바로가기</a>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 export default WorksList;
