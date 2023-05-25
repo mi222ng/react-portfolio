03993c44b90707c419809e68fac8547d96fa616c
mi222ng <mi222ng@naver.com>
2023-05-25 18:50:15 +0900

    새창으로 열기로 변경

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub,faInstagram } from '@fortawesome/free-brands-svg-icons';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 class Works extends Component {
   render() {
     return (
-      <section className='section' id='contact'>
-        <h2 className='title'>Contact me here!<span role="img" aria-label="Backhand Index Pointing Down">👇</span></h2>
-        <div className='d-flex justify-content-center'>
-          <a href='https://github.com/mi222ng'><FontAwesomeIcon icon={faGithub} beatFade className='contacticon mygit'/></a>
-          <a href='https://www.instagram.com/mi222ng'><FontAwesomeIcon icon={faInstagram} beatFade className='contacticon myinsta'/></a>
+      <section className="section" id="contact">
+        <h2 className="title">
+          Contact me here!
+          <span role="img" aria-label="Backhand Index Pointing Down">
+            👇
+          </span>
+        </h2>
+        <div className="d-flex justify-content-center">
+          <a href="https://github.com/mi222ng" target="_blank">
+            <FontAwesomeIcon
+              icon={faGithub}
+              beatFade
+              className="contacticon mygit"
+            />
+          </a>
+          <a href="https://www.instagram.com/mi222ng" target="_blank">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              beatFade
+              className="contacticon myinsta"
+            />
+          </a>
           {/* <p>hello</p> */}
         </div>
       </section>
-    )
+    );
   }
 }
 
